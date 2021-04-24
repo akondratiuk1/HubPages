873700db23adf90fa2abc839c23125cd4f5198ec
Anton Kondratiuk <akondratiuk@playtika.com>
2021-04-24 12:43:43 +0300

    git ignore

--- myApp/src/index.ts
+++ myApp/src/index.ts
@@ -1,8 +1,10 @@
+declare let cordova:any; 
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + 'cordova.platformId' + '@' + 'cordova.version');
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
